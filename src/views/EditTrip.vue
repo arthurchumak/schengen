5cd34ef6252264649d0a9efbb5ac7a8b0cb40d7a
Arthur Chumak <arthurchumak@gmail.com>
2019-01-10 06:58:29 +0300

    fix editing

--- src/views/EditTrip.vue
+++ src/views/EditTrip.vue
@@ -1,50 +1,51 @@
 <template>
-    <div>
-        <h1>Редактор</h1>
+  <div>
+    <h1>Редактор</h1>
 
-        <div v-if="trip">
-            <input type="date" v-model="trip.dates[0]" :max="trip.dates[1]">
-            <Days :start="trip.dates[0]" :end="trip.dates[1]"/>
-            <input type="date" v-model="trip.dates[1]" :min="trip.dates[0]">
-        </div>
-        <div v-else>Not Found</div>
-
-        <button @click="save">Сохранить</button>
-        <button @click="remove">Удалить</button>
+    <div v-if="dates">
+      <input type="date" v-model="dates[0]" :max="dates[1]">
+      <Days :start="dates[0]" :end="dates[1]"/>
+      <input type="date" v-model="dates[1]" :min="dates[0]">
     </div>
+    <div v-else>Not Found</div>
+
+    <button @click="save">Сохранить</button>
+    <button @click="remove">Удалить</button>
+  </div>
 </template>
 
 <script>
 import Days from "@/components/Days";
-import {SchengenCalculator} from "@/Calculator/Shengen"
-import {mapGetters} from "vuex";
+import { SchengenCalculator } from "@/Calculator/Shengen";
+import { mapGetters } from "vuex";
 
 export default {
-    props: ['title'],
-    components: {Days},
-    created() {
-        this.trip = this.sdk.getTrip(this.$route.params.id)
+  props: ["title"],
+  components: { Days },
+  created() {
+    this.sdk.getTrip(this.$route.params.id).once("value", snapshot => {
+      this.dates = snapshot.val();
+    });
+  },
+  data() {
+    return {
+      dates: []
+    };
+  },
+  methods: {
+    save() {
+      this.sdk.updateTrip(this.$route.params.id, this.dates);
+      this.$router.go(-1);
     },
-    data() {
-        return {
-            trip: null,
-        };
-    },
-    methods: {
-        save() {
-            this.sdk.updateTrip(this.trip.id, this.trip.dates);
-            this.$router.go(-1);
-        },
-        remove() {
-            if (confirm('This will permanently delete the trip. Continue?')) {
-                this.sdk.removeTrip(this.trip.id);
-                this.$router.go(-1);
-            }
-        }
+    remove() {
+      if (confirm("This will permanently delete the  Continue?")) {
+        this.sdk.removeTrip(this.$route.params.id);
+        this.$router.go(-1);
+      }
     }
-}
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
